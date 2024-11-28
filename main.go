d032a14e6ccb649ff16ba01cc7d1f2780027cd1a
Jasmine <user@Jasmines-MacBook.local>
2024-11-28 22:11:44 +0700

    update cors

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"backend/routes"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
@@ -12,6 +13,17 @@ func main() {
 
 	// Initialize Database
 	config.InitDB()
+
+	// Apply CORS middleware with custom config
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowHeaders: []string{
+			echo.HeaderOrigin,
+			echo.HeaderContentType,
+			echo.HeaderAccept,
+			echo.HeaderAuthorization,
+		},
+	}))
 
 	// Register Routes
 	routes.RegisterRoutes(e)
